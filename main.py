a1eb00877fd1c8a497c3cfd23e2e2c181a2f1c8f
AlexTomagan <98933025+AlexTomagan@users.noreply.github.com>
2022-04-11 12:41:00 +0800

    Add files via upload

--- main.py
+++ main.py
@@ -1,25 +1,11 @@
 from tkinter import *
 window = Tk()
 
-def click():
-    print("Alexander A Tomagan")
-
 window.geometry("400x150+10+20")
-window.title("Program 2")
+window.title("Text Field")
 
-label=Label(window,text = "Enter your fullname:",  fg = "red")
-label.place(x=90,y=170)
-
-txtfld = Entry(window,text = "Alexander A Tomagan", bd = 5)
-txtfld.place(x=210,y=170)
-
-button=Button(window,text = "click to display your full name", fg = "red")
-button.config(command=click) #perform call back of function
-button.place(x=40,y=200)
-
-txtfld = Entry(window,text = "Alexander A Tomagan", bd = 5)
-txtfld.place(x=210,y=200)
+txtfld = Entry(window,text = "This is where i type my text", bd = 5)
+txtfld.place(x=100,y=100)
 window.mainloop()
-
 
 
